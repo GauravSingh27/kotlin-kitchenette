7357796fe17e952aa05a4c9015c91bcec8d8fdb5
iamGauravSingh <singh27gaurav@gmail.com>
2018-01-15 13:36:24 +0530

    Update NamedArgFunction.kt

--- src/functions/NamedArgFunction.kt
+++ src/functions/NamedArgFunction.kt
@@ -4,12 +4,14 @@ package functions
 /**
  * With named arguments we can make the code much more readable
  */
-fun namedArgumentFunction(firstName: String, lastName: String, age: Int) {
+fun namedArgumentFunction(name: String = "John", company: String, country: String = "India") {
 
-    println("Hi $firstName $lastName")
+    println("$name works at $company in $country")
 }
 
 fun main(args: Array<String>) {
 
-    namedArgumentFunction(firstName = "Gaurav", lastName = "Singh")
+   namedArgumentFunction(name = "Trump", company = "TCS", country = "US")
+   namedArgumentFunction(company = "TCS", name = "Mike")
+   namedArgumentFunction(company = "TCS")
 }
